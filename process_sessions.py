67191349888286fa518fa8a062c4d3adf438a236
Alexander Hendorf <hendorf@opotoc.com>
2019-08-11 23:13:13 +0200

    added schedule with css and databags, all depends on Google Sheet now

--- process_sessions.py
+++ process_sessions.py
@@ -57,10 +57,17 @@ def get_all_data_from_pretalx(url, params=None):
 
 def load_submissions(accepted_only=True):
     url = f'{base_url}/api/events/{event}/submissions/'
-    if accepted_only:
-        # duplicate param 'state' cannot be passed via params dict
-        url = f'{url}?q=&state=accepted&state=confirmed'
-    submissions = get_all_data_from_pretalx(url)
+    if not accepted_only:
+        submissions = get_all_data_from_pretalx(url)
+    else:
+        submissions = get_all_data_from_pretalx(url, params={'state': 'accepted'})
+        submissions.extend(get_all_data_from_pretalx(url, params={'state': 'confirmed'}))
+    # add custom data
+    for submission in submissions:
+        spkrs = ' '.join([x.get('name') for x in submission['speakers']])
+        slug = slugify(f"{submission.get('track', {}).get('en', 'pycon-pydata')}-{submission['code']}-{submission['title']}-{spkrs}")
+        submission['slug'] = slug
+
     with open(submissions_path, 'w') as f:
         json.dump(submissions, f, indent=4)
 
@@ -86,6 +93,10 @@ def load_speakers():
             if _id not in qa_map:
                 continue
             speaker[qa_map[_id]] = qa.get('answer')
+            # normalize twitter
+            if _id == 115:
+                speaker[qa_map[_id]] = qa.get('answer').split('/')[-1].replace('@', '')
+                speaker['twitter'] = f"https://twitter.com/{speaker[qa_map[_id]]})"
         the_speakers.append(speaker)
     with open(speakers_path, 'w') as f:
         json.dump(the_speakers, f, indent=4)
@@ -120,175 +131,9 @@ def format_date(dt):
     return dt.strftime("%A %H:%M").lower()
 
 
-def get_talk_identifier(s):
-    return s.split(" ", 1)[0]
-
-
-def gen_schedule_talks(data):
-    tpl = """_model: talk 
----
-code: {}
-"""
-    for e in data.values():
-        dirname = "pyconde/content/schedule/talks/{}".format(e["slug"])
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        with open(os.path.join(dirname, "contents.lr"), "w") as f:
-            f.write(tpl.format(e["slot_code"]))
-    return data
-
-
 def gen_gravatar(email):
     h = hashlib.md5(email.encode("utf-8")).hexdigest()
     return "https://www.gravatar.com/avatar/{}".format(h)
-
-
-def fix_data(data):
-    for talk in data:
-        for speaker in talk.get("the_speakers", []):
-            speaker["gravatar"] = gen_gravatar(speaker["email"])
-            if "homepage" in speaker and not speaker["homepage"].strip().startswith(
-                    "http:"
-            ):
-                speaker["homepage"] = "http://{}".format(speaker["homepage"].strip())
-    return data
-
-
-def load_talks_metadata(data, filename=None):
-    return json.load(open(filename))
-
-
-def merge_schedule(data, schedule=None):
-    accepted_talks = {}
-
-    for talk in schedule:
-        for d in data:
-            if talk["talk_code"] == d["code"]:
-                d["slot_time"] = talk["slot_time"]
-                d["slot_date"] = talk["slot_date"]
-                d["slot_code"] = "{}-{}".format(talk["slot_time"], talk["room"])
-                d["room"] = talk["room"]
-                d["slug"] = slugify(d["title"])
-                accepted_talks[d["slot_code"]] = d
-    return accepted_talks
-
-
-def drop_values(data):
-    for talk in data:
-        for speaker in talk.get("the_speakers", []):
-            pass
-    return data
-
-
-def load_schedule(filename=None):
-    rows = [
-        3,
-        4,
-        6,
-        7,
-        9,
-        10,
-        11,
-        13,
-        14,
-        15,
-        17,
-        18,
-        19,
-        21,
-        22,
-        23,
-        25,
-        26,
-        27,
-        29,
-        30,
-        31,
-    ]
-
-    rooms = {"B": "cubus", "C": "media", "D": "lounge", "E": "lecture", "G": "openhub"}
-
-    wb = load_workbook(filename)
-    sheet = wb["Sheet1"]
-    sheet["G9"].value = "8WXEH8 {}".format(sheet["G9"].value)
-    talk_list = []
-    for row in rows:
-        for room, room_name in rooms.items():
-            slot_time = date2identifier(sheet[f"A{row}"].value)
-            slot_date = format_date(sheet[f"A{row}"].value)
-            talk = sheet[f"{room}{row}"].value
-            if talk is None or talk.strip() == "" or "<<< available >>>" in talk:
-                continue
-            talk_identifier = get_talk_identifier(talk)
-            talk_list.append(
-                {
-                    "slot_time": slot_time,
-                    "slot_date": slot_date,
-                    "room": room_name,
-                    "talk_code": talk_identifier,
-                    "talk": talk,
-                }
-            )
-    return talk_list
-
-
-def drop_sensitive_speaker_infromation(data):
-    for talk in data.values():
-        for speaker in talk.get("the_speakers", []):
-            for k in list(speaker.keys()):
-                if not k in ["name", "biography", "homepage", "@twitter"]:
-                    del speaker[k]
-    return data
-
-
-def load_videos(filename):
-    d = {}
-    for video in json.load(open(filename)):
-        d[video["code"]] = video
-    return d
-
-
-def write_databag(data):
-    json.dump(data, open("pyconde/databags/talks.json", "w"), indent=4)
-    return data
-
-
-def merge_video(data, videos):
-    for talk in data.values():
-        talk["video"] = videos.get(talk["code"], None)
-    return data
-
-
-def pipe(*tasks):
-    data = None
-    for task in tasks:
-        data = task(data)
-    return data
-
-
-@click.command()
-@click.option(
-    "--schedule_dir",
-    envvar="SCHEDULE_DIR",
-    default="../../github_pycon_priv/2018/",
-    help="The directory with the schedule and the speaker json",
-)
-def cli(schedule_dir):
-    """generate the databag for the schedule"""
-
-    xsl_file = os.path.join(schedule_dir, "pyconde18-pydata-ka-schedule.xlsx")
-    json_file = os.path.join(schedule_dir, "pyconde18-pydata-ka-all_submissions.json")
-    video_file = os.path.join(schedule_dir, "videos/video_schedule_mapping.json")
-
-    data = pipe(
-        partial(load_talks_metadata, filename=json_file),
-        fix_data,
-        partial(merge_schedule, schedule=load_schedule(xsl_file)),
-        drop_sensitive_speaker_infromation,
-        partial(merge_video, videos=load_videos(video_file)),
-        write_databag,
-        gen_schedule_talks,
-    )
 
 
 def update_session_pages(use_cache=False):
@@ -307,7 +152,7 @@ def update_session_pages(use_cache=False):
     # take on only required attributes
 
     eq_attr = ['abstract', 'answers', 'code', 'description', 'duration', 'is_featured',
-               'speakers', 'state', 'submission_type', 'title', 'track']
+               'speakers', 'state', 'submission_type', 'title', 'track', 'slug']
     id_answers = {118: 'short_description', 111: 'python_skill', 110: 'domain_expertise', 119: 'domains'}
     cleaned_submissions = []
     for s in submissions:
@@ -324,7 +169,7 @@ def update_session_pages(use_cache=False):
         # add speaker info
         enriched_speakers = []
         for x in cs['speakers']:
-            take_on = ['affiliation', 'homepage', '@twitter', 'github', 'biography']
+            take_on = ['affiliation', 'homepage', '@twitter', 'twitter', 'github', 'biography']
             _add = {k: speakers[x['code']].get(k, '') for k in take_on}
             x.update(_add)
             enriched_speakers.append(x)
@@ -433,8 +278,8 @@ body: {body}
             biography.append(f'')
             biography.append(f"{x['biography']}")
             social = []
-            if x.get('@twitter'):
-                social.append(f"[Twitter](https://twitter.com/{x['@twitter'].replace('@', '')})")
+            if x.get('twitter'):
+                social.append(f"[Twitter]({x['twitter']})")
             if x.get('github'):
                 social.append(f"[Github]({x['github']})")
             if x.get('homepage'):
@@ -444,7 +289,7 @@ body: {body}
         biography = '\n\n'.join(biography)
 
         speaker_twitters = ' '.join([x.get('@twitter') for x in submission['speakers'] if x.get('@twitter')])
-        meta_title = f"{submission['title']} {speaker_twitters} #PyConDE #PyDataBerlin"
+        meta_title = f"{submission['title']} {speaker_twitters} #PyConDE #PyDataBerlin #PyData"
 
         # easier to handle on website as full text
         python_skill = f"Python Skill Level {submission['python_skill']}"
@@ -457,28 +302,16 @@ body: {body}
         categories_list = ', '.join(slugified_categories)
         all_categories.update({slugify(x): x for x in categories})
 
-        slug = slugify(f"{submission['track']}-{submission['code']}-{submission['title']}-{' '.join([x.get('name') for x in submission['speakers']])}")
-
-        redirects[submission['code']] = slug
+        redirects[submission['code']] = submission['slug']
         redir_dirname = session_path / submission['code']
         if submission['code'] in in_place_submissions:
             in_place_submissions.remove(submission['code'])
+        create_redirect(redir_dirname, submission['slug'])
 
-        redir_dirname.mkdir(exist_ok=True)
-        with open(redir_dirname / "contents.lr", "w") as f:
-            f.write("""_model: redirect
----
-target: /program/{}
----
-_discoverable: no""".format(slug))
-
-        dirname = session_path / slug
+        dirname = session_path / submission['slug']
         if dirname.name in in_place_submissions:
             # print("slug hasn't changed")
             in_place_submissions.remove(dirname.name)
-        else:
-            # TODO redirect renames to new dir
-            raise NotImplementedError("handling of redirects not implemneted, yet")
 
         dirname.mkdir(exist_ok=True)
         with open(dirname / "contents.lr", "w") as f:
@@ -502,6 +335,7 @@ _discoverable: no""".format(slug))
             ))
     if in_place_submissions:  # leftover dirs
         for zombie in in_place_submissions:
+            # TODO could try to redirect zombies via code
             zpath = Path('pyconde/content/program/') / zombie
             shutil.rmtree(zpath)
 
@@ -518,9 +352,20 @@ description: All {0} sessions at the PyConDE & Pydata Berlin 2019 conference
 ---""".format(all_categories[category]))
 
 
+def create_redirect(redir_dirname, slug):
+    redir_dirname.mkdir(exist_ok=True)
+    with open(redir_dirname / "contents.lr", "w") as f:
+        f.write("""_model: redirect
+---
+target: /program/{}
+---
+_discoverable: no""".format(slug))
+
+
 if __name__ == "__main__":
     # load_speakers()
     # update_session_pages(use_cache=True)
+    update_session_pages(use_cache=False)
     # save_csv_for_banners()
     # rename_tmp_banners()
     generate_session_pages()
