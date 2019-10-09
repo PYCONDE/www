16c8df20f33f1a63aa2680a15ef603564567668e
Alexander Hendorf <hendorf@gmail.com>
2019-10-09 12:31:11 +0200

    website-auto-update

--- schedule/checkForRecordingOptOut.py
+++ schedule/checkForRecordingOptOut.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 import json
 
-with open('/Users/hendorf/Documents/code/PyConDE-www/_private/submissions.json') as f:
+with open('/Users/hendorf/code/PyConDE-www/_private/submissions.json') as f:
     data = json.load(f)
 
 opt_out = [(x['title'], x['state']) for x in data if x['do_not_record']]
